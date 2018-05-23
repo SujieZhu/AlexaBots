9fd7af5a7b0c80e9fcb7de404d5ffa8a8df999bf
QI HU <qihucn@uw.edu>
2018-05-22 17:06:40 -0700

    add comments for parameters and return values

--- utils/query_api.py
+++ utils/query_api.py
@@ -22,15 +22,28 @@ def search_google(keyword, location='', radius=8000, types=['restaurant',], limi
        (based on https GET request, no need for googleplaces package).
 
             Args:
-                keyword: [string]
-                location [string]: The latitude/longitude, e.g. '47.606210, -122.332070'
-                radius: [string]
-                types
-                limit
-                api_key
+                keyword: [str] You can put food type and location here
+                location: [str] The latitude/longitude, e.g. '47.606210, -122.332070' (leave it blank if you don't have this)
+                radius: [str] The distance constraint given the location(latitude/longitude)
+                types: [list of str] the type of business we are going to search (the default value is 'restaurant')
+                limit: [int] number of items in query result
+                api_key: [str] KEY for api query (the default value should work)
 
             Returns:
-                places: The JSON response from the request.
+                places: A list of restaurants information (in JSON format).
+                    formatted_address: [str] address in [Street No., City, State Zipcode, Country] format
+                    geometry: [dict] location(latitude, longitude), viewport: (not sure what is this for)
+                    icon: [str] a url to the restaurant's icon
+                    name: [str]
+                    opening_hours: [dict] open_now[boolean], weekday_text
+                    photos: [list] urls for photos
+                    place_id [str]: used for detail information (get_google_detail())
+                    id: [str] (not sure, ignore it for now)
+                    price_level: [int] 1-5, the larger this number is, the more expensive
+                    rating: [double] 0-5, the larger this number is, the better
+                    reference: [str] (not sure what is this for)
+                    types [list] categories this restaurant belongs to
+                    detail (information of this attribute is in the function get_google_detail)
     """
     url_params = {
         'query': keyword.replace(' ', '+'),
@@ -53,11 +66,34 @@ def get_google_detail(placeid, api_key=GOOGLE_KEY):
     """Get the detail information for a Google Place
 
                 Args:
-                    placeid: [string]
-                    api_key: [string]
+                    placeid: [str] place_id get from search_google, to retrieval a specific restaurant
+                    api_key: [str]
 
                 Returns:
-                    detail: The JSON response for place detail
+                    detail: The JSON response for place detail information
+                        address_components: [dict] JSON format address
+                        adr_address: [str] a xml format address
+                        formatted_address: [str]
+                        formatted_phone_number: [str] (206) 557-7532
+                        geometry: [dict] location(latitude, longitude), viewport
+                        icon [str]: a url to the icon of the restaurant
+                        international_phone_number: [str] phone number with international area code as prefix
+                        name: [str]
+                        opening_hours: [dict] open_now[boolean], periods(close, open)
+                        reviews: [list] reviews together with the author information
+                        photos: [list] several urls for photos
+                        place_id: [str] id to retrieval the restaurant
+                        id: [str] (not sure, ignore it for now)
+                        vicinity: [str] name of the area
+                        price_level
+                        rating
+                        reference
+                        scope
+                        types
+                        url
+                        utc_offset
+                        website
+
     """
     url_params = {
         'placeid': placeid.replace(' ', '+'),
@@ -101,12 +137,27 @@ def search_yelp(keyword, location, api_key = YELP_KEY, limit=1, open_at=None, op
     """Query the YELP Search API by a search term and location.
 
     Args:
-        term (str): The search term passed to the API.
-        location (str): The search location passed to the API.
-        open_at (int): The time-value passed to the API. Cannot be set in conjunction with open_now; if it is, function defaults to open_now.
-        open_now (bool): The bool val passed to the API. Cannot be set in conjunction with open_at; if it is, function defaults to open_now.
+        term: [str] The search term passed to the API.
+        location: [str] The search location passed to the API.
+        open_at: [int] The time-value passed to the API. Cannot be set in conjunction with open_now; if it is, function defaults to open_now.
+        open_now: [bool] The bool val passed to the API. Cannot be set in conjunction with open_at; if it is, function defaults to open_now.
     Returns:
         dict: The JSON response from the request.
+            id: [str]
+            alias: [str]
+            name: [str]
+            image_url: [str]
+            is_closed: [bool]
+            url: [str]
+            review_count: [int]
+            categories: [list]
+            rating: [double]
+            coordinates: [dict] latitude & longitude
+            price: [str]  several '$'s
+            location: [dict] formatted
+            phone: [str]
+            display_phone: [str]
+            distance [double]
     """
 
     try: assert not(open_at and open_now)
@@ -125,34 +176,10 @@ def search_yelp(keyword, location, api_key = YELP_KEY, limit=1, open_at=None, op
 
     places = request(YELP_SEARCH_PATH, api_key, url_params=url_params)
     return places['businesses']
-    '''YELP Places Attributes 
-        region
-          center
-              latitude
-              longitude
-        total
-        businesses: (a list)
-          rating
-          review_count
-          name
-          transactions
-          url
-          price
-          distance
-          coordinates
-          alias
-          image_url
-          categories
-          display_phone
-          phone
-          id
-          is_closed
-          location
-        '''
 
 
 def search_yelp_business(business_id, api_key=YELP_KEY):
-    """Query the YELP Business API by a business ID.
+    """Query the YELP Business API by a business ID. (This is currently not used in our project)
 
     Args:
         business_id (str): The ID of the business to query.
@@ -192,7 +219,7 @@ def request(path, api_key, url_params=None):
 
 if __name__ == '__main__':
     google_places = search_google(keyword='Seattle seafood', location='47.606210, -122.332070', radius='8000')  # location='47.606210, -122.332070', radius=8000
-    yelp_places = search_yelp(keyword='seafood', location='Seattle', limit=1, radius=5000)
+    yelp_places = search_yelp(keyword='seafood', location='Seattle', limit=10, radius=5000)
     direction = get_google_direction('driving', 'Seattle', 'Portland')
 
     print('Direction: \n', direction)
